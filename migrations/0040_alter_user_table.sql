ed9063e746698bb741c1de0529c1f8aba146ac56
Craig Saunders <craig.saunders@smoothstack.com>
2021-03-20 04:54:57 -0400

    Remove is from is_active

--- migrations/0040_alter_user_table.sql
+++ migrations/0040_alter_user_table.sql
@@ -1,5 +1,4 @@
 --liquibase formatted sql
 --changeset craig:alter-user-table
-ALTER TABLE `utopia`.`user` 
-ADD COLUMN `is_active` TINYINT NOT NULL DEFAULT 0 AFTER `phone`;
- --rollback ALTER TABLE `utopia`.`user` DROP COLUMN `is_active`;
+ALTER TABLE `utopia`.`user` ADD COLUMN `active` TINYINT(1) UNSIGNED NOT NULL DEFAULT 0 AFTER `phone`;
+ --rollback ALTER TABLE `utopia`.`user` DROP COLUMN `active`;
